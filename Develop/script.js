8fb4db077434243160473af6609e88c99f43bf69
palminski <williambolls98@gmail.com>
2022-07-29 20:36:16 -0700

    collected input

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,57 @@
 // Assignment code here
+let passwordParameters = {
+  length : 0,
+  lowercase : null,
+  uppercase : null,
+  numbers : null,
+  specialCharacters : null,
+}
+
+
+function generatePassword() {
+  window.alert("Starting password generation");
+  passwordParameters.length = determinePasswordLength();
+  determineCharacterTypes();
+  //Debugging Stuff
+  console.log ("lenght selected - " + passwordParameters.length);
+  if (passwordParameters.lowercase) {
+    console.log("Lowercase");
+  }
+  if (passwordParameters.lowercase) {
+    console.log("Uppercase");
+  }
+  if (passwordParameters.lowercase) {
+    console.log("Numbers");
+  }
+  if (passwordParameters.lowercase) {
+    console.log("Special characters");
+  }
+}
+
+function determinePasswordLength() {
+  let length = null;
+  let confirm = true;
+  while (length < 8 || length > 128 || Number.isNaN(length) || !confirm) {
+    length = parseInt(window.prompt("Enter the length of desired password. Must be between 8 and 128. Decimals will be rounded down"));
+    if (length < 8 || length > 128 || Number.isNaN(length)) {
+      window.alert("Please input an whole number value between 8 and 128");
+    }
+    confirm = (window.confirm("Are you sure a length of " + length + " is okay?"))
+  }
+  return length
+}
+function determineCharacterTypes() {
+  let confirm = false;
+  while (!confirm) {
+    passwordParameters.lowercase = window.confirm("include lowercase characters?");
+    passwordParameters.uppercase = window.confirm("include uppercase characters?");
+    passwordParameters.numbers = window.confirm("include numeric characters?");
+    passwordParameters.specialCharacters = window.confirm("include special characters?");
+    confirm = (window.confirm("Are you sure your selection is okay? " + 
+    passwordParameters.lowercase + passwordParameters.uppercase + passwordParameters.numbers + passwordParameters.specialCharacters));
+  }
+
+}
 
 
 // Get references to the #generate element
@@ -15,3 +68,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
