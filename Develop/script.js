da5cfafb74fce7f0c1cc78c37539824fbc80a578
palminski <williambolls98@gmail.com>
2022-07-29 21:25:24 -0700

    collecting all data properly and selecting an array to assemble password from

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,10 @@ let passwordParameters = {
   numbers : null,
   specialCharacters : null,
 }
+let arrayLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let arrayUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let arrayNumbers = [1,2,3,4,5,6,7,8,9,0];
+let arraySpecialCharacters = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","}","|","~"];
 
 
 function generatePassword() {
@@ -17,16 +21,16 @@ function generatePassword() {
   if (passwordParameters.lowercase) {
     console.log("Lowercase");
   }
-  if (passwordParameters.lowercase) {
+  if (passwordParameters.uppercase) {
     console.log("Uppercase");
   }
-  if (passwordParameters.lowercase) {
+  if (passwordParameters.numbers) {
     console.log("Numbers");
   }
-  if (passwordParameters.lowercase) {
+  if (passwordParameters.specialCharacters) {
     console.log("Special characters");
   }
-
+  return assemblePassword();
 }
 
 function determinePasswordLength() {
@@ -37,24 +41,68 @@ function determinePasswordLength() {
     if (length < 8 || length > 128 || Number.isNaN(length)) {
       window.alert("Please input an whole number value between 8 and 128");
     }
-    confirm = (window.confirm("Are you sure a length of " + length + " is okay?"))
+    if (!Number.isNaN(length)){
+      confirm = (window.confirm("Are you sure a length of " + length + " is okay?"))
+    }
   }
   return length
 }
 function determineCharacterTypes() {
   let confirm = false;
-  while (!confirm) {S
+  let selectedCharacterTypes = "";
+  while (!confirm) {
     passwordParameters.lowercase = window.confirm("include lowercase characters?");
     passwordParameters.uppercase = window.confirm("include uppercase characters?");
     passwordParameters.numbers = window.confirm("include numeric characters?");
     passwordParameters.specialCharacters = window.confirm("include special characters?");
-    confirm = (window.confirm("Are you sure your selection is okay? " + 
-    passwordParameters.lowercase + passwordParameters.uppercase + passwordParameters.numbers + passwordParameters.specialCharacters));
+
+    if (passwordParameters.lowercase) {
+      selectedCharacterTypes += "[Lowercase], ";
+    }
+    if (passwordParameters.uppercase) {
+      selectedCharacterTypes += "[Uppercase], ";
+    }
+    if (passwordParameters.numbers) {
+      selectedCharacterTypes += "[Numbers], ";
+    }
+    if (passwordParameters.specialCharacters) {
+      selectedCharacterTypes += "[Special Characters], ";
+    }
+    confirm = (window.confirm("You have selected " + selectedCharacterTypes + "correct?"));
   }
 
 }
 
+function assemblePassword() {
+  let finalPassword = "";
+  let arrayPasswordElements = [];
+  if (passwordParameters.lowercase) {
+    arrayPasswordElements.push(arrayLowercase);
+  }
+  if (passwordParameters.uppercase) {
+    arrayPasswordElements.push(arrayUppercase);
+  }
+  if (passwordParameters.numbers) {
+    arrayPasswordElements.push(arrayNumbers);
+  }
+  if (passwordParameters.specialCharacters) {
+    arrayPasswordElements.push(arraySpecialCharacters);
+  }
 
+  console.log (arrayPasswordElements);
+
+  for (i=1;i<=passwordParameters.length;i++) {
+    finalPassword += "0";
+  }
+  return finalPassword;
+}
+
+function randomRange() {
+  
+}
+
+
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
