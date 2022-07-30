f1d0f8df2458fc92172aad2378712df0ff73e940
palminski <williambolls98@gmail.com>
2022-07-29 21:52:07 -0700

    finished password generator

--- Develop/script.js
+++ Develop/script.js
@@ -10,32 +10,20 @@ let arrayLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o
 let arrayUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let arrayNumbers = [1,2,3,4,5,6,7,8,9,0];
 let arraySpecialCharacters = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","}","|","~"];
+let arrayPasswordElements = [];
 
 
 function generatePassword() {
   window.alert("Starting password generation");
   passwordParameters.length = determinePasswordLength();
   determineCharacterTypes();
-  //Debugging Stuff
-  console.log ("lenght selected - " + passwordParameters.length);
-  if (passwordParameters.lowercase) {
-    console.log("Lowercase");
-  }
-  if (passwordParameters.uppercase) {
-    console.log("Uppercase");
-  }
-  if (passwordParameters.numbers) {
-    console.log("Numbers");
-  }
-  if (passwordParameters.specialCharacters) {
-    console.log("Special characters");
-  }
   return assemblePassword();
 }
 
 function determinePasswordLength() {
   let length = null;
   let confirm = true;
+
   while (length < 8 || length > 128 || Number.isNaN(length) || !confirm) {
     length = parseInt(window.prompt("Enter the length of desired password. Must be between 8 and 128. Decimals will be rounded down"));
     if (length < 8 || length > 128 || Number.isNaN(length)) {
@@ -50,55 +38,54 @@ function determinePasswordLength() {
 function determineCharacterTypes() {
   let confirm = false;
   let selectedCharacterTypes = "";
-  while (!confirm) {
+  arrayPasswordElements = [];
+
+  while (!confirm || arrayPasswordElements.length<1) {
     passwordParameters.lowercase = window.confirm("include lowercase characters?");
     passwordParameters.uppercase = window.confirm("include uppercase characters?");
     passwordParameters.numbers = window.confirm("include numeric characters?");
     passwordParameters.specialCharacters = window.confirm("include special characters?");
-
+    selectedCharacterTypes = "";
+    arrayPasswordElements = [];
     if (passwordParameters.lowercase) {
       selectedCharacterTypes += "[Lowercase], ";
+      arrayPasswordElements.push(arrayLowercase);
     }
     if (passwordParameters.uppercase) {
       selectedCharacterTypes += "[Uppercase], ";
+      arrayPasswordElements.push(arrayUppercase);
     }
     if (passwordParameters.numbers) {
       selectedCharacterTypes += "[Numbers], ";
+      arrayPasswordElements.push(arrayNumbers);
     }
     if (passwordParameters.specialCharacters) {
       selectedCharacterTypes += "[Special Characters], ";
+      arrayPasswordElements.push(arraySpecialCharacters);
     }
-    confirm = (window.confirm("You have selected " + selectedCharacterTypes + "correct?"));
+    if (arrayPasswordElements <= 1) {
+      window.alert("You must include at least one character type to procede.");
+    }
+    else{
+      confirm = (window.confirm("You have selected " + selectedCharacterTypes + "correct?"));
+    }
   }
-
 }
 
 function assemblePassword() {
   let finalPassword = "";
-  let arrayPasswordElements = [];
-  if (passwordParameters.lowercase) {
-    arrayPasswordElements.push(arrayLowercase);
-  }
-  if (passwordParameters.uppercase) {
-    arrayPasswordElements.push(arrayUppercase);
-  }
-  if (passwordParameters.numbers) {
-    arrayPasswordElements.push(arrayNumbers);
-  }
-  if (passwordParameters.specialCharacters) {
-    arrayPasswordElements.push(arraySpecialCharacters);
-  }
-
-  console.log (arrayPasswordElements);
+  let currentArray = [];
 
   for (i=1;i<=passwordParameters.length;i++) {
-    finalPassword += "0";
+    currentArray = arrayPasswordElements[randomRange(0,arrayPasswordElements.length-1)];
+    finalPassword += currentArray[randomRange(0,currentArray.length-1)];
   }
   return finalPassword;
 }
 
-function randomRange() {
-  
+function randomRange(min,max) {
+  let randomValue = Math.floor( Math.random() * (max - min +1) + min);
+  return randomValue;
 }
 
 
